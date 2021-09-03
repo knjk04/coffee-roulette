3ea81f0ae7ce0ba30d1d7b61c8cba96f911e0a1d
Karan <karan.n.j.kumar@gmail.com>
2021-09-03 17:29:59 +0100

    Update app module

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -5,16 +5,24 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import {MatToolbarModule} from "@angular/material/toolbar";
+import { FileUploadComponent } from './file-upload/file-upload.component';
+import {MatIconModule} from "@angular/material/icon";
+import {HttpClientModule} from "@angular/common/http";
+import { HeaderComponent } from './header/header.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FileUploadComponent,
+    HeaderComponent
   ],
     imports: [
         BrowserModule,
         AppRoutingModule,
         BrowserAnimationsModule,
-        MatToolbarModule
+        MatToolbarModule,
+        MatIconModule,
+        HttpClientModule
     ],
   providers: [],
   bootstrap: [AppComponent]
