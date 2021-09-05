23c2f79d4238ff16c16dedd59994303d3c04715f
Karan <karan@karankumar.com>
2021-09-05 15:25:08 +0100

    Add more tests for pair_people

--- backend/tests/test_pair_people.py
+++ backend/tests/test_pair_people.py
@@ -21,4 +21,29 @@ class TestPairPeople(unittest.TestCase):
         with self.assertRaises(ValueError):
             pair({'Arthur'})
 
-    # TODO: assert that there is one group of three if there are an odd number of people to pair
+
+    def test_one_group_of_three_for_3_people(self):
+        odd_number_of_people= {'Alice', 'Bob', 'Claire'}
+        pairings = pair(odd_number_of_people)
+        self.assertEqual(1, len(pairings))
+
+
+    def test_one_three_for_5_people(self):
+        odd_number_of_people= {'Alice', 'Bob', 'Claire', 'Dave', 'Eve'}
+        pairings = pair(odd_number_of_people)
+
+        first = pairings[0]
+        second = pairings[1]
+
+        self.assertTrue(len(first) == 3 or len(second) == 3)
+
+
+    def test_no_threes_for_even_number_of_people(self):
+        odd_number_of_people= {'Alice', 'Bob', 'Claire', 'Dave'}
+        pairings = pair(odd_number_of_people)
+
+        first_pair = pairings[0]
+        second_pair = pairings[1]
+
+        self.assertEqual(2, len(first_pair))
+        self.assertEqual(2, len(second_pair))
